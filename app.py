aa140074ed2a8761682a3c41f947df97545c60ff
mchocianowski <ch.mateusz@hotmail.com>
2021-04-23 14:36:07 +0000

    add werkzeug library. define new variable: register.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def get_games():
     games = mongo.db.games.find()
     return render_template("games.html", games=games)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
