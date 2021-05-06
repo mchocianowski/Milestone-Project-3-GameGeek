2c3cbd9be726a91340b6bab5cc88e132a2f2fb6b
mchocianowski <ch.mateusz@hotmail.com>
2021-05-06 18:36:41 +0000

    create login function in app.py

--- app.py
+++ app.py
@@ -49,6 +49,30 @@ def register():
         return redirect(url_for("profiles", username=session["user"]))
     return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profiles", username=session["user"]))
+            else:
+                flash("It seems that the Username or Password entered are incorrect, please try again.")
+                return redirect(url_for("login"))
+
+        else:
+            flash(
+                "It seems that the Username or Password entered are incorrect, please try again.")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
